50a1ff2cb09e8b97e9eba620f329c3d8d38ab594
Jakub Sumionka <owiec2000@gmail.com>
2021-04-03 19:41:41 +0200

    Initial Commit

---
+++ Swift Student Challenge-XCode/Swift_Student_Challenge_XCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Student_Challenge_XCodeApp.swift
+//  Swift Student Challenge-XCode
+//
+//  Created by Jakub Sumionka on 03/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Student_Challenge_XCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
