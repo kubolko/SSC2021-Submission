5bc2caec51125263131b26f371027ac12c16802f
kubolko <jakub.sumionka@gmail.com>
2021-04-18 11:05:28 +0200

    end of this

--- Swift Student Challenge-XCode/Backlog.md
+++ Swift Student Challenge-XCode/Backlog.md
@@ -13,5 +13,36 @@ Gra:
 
 https://stackoverflow.com/questions/38865788/moving-camera-in-spritekit-swift
 
+func linesCross(start1: CGPoint, end1: CGPoint, start2: CGPoint, end2: CGPoint) -> (x: CGFloat, y: CGFloat)? {
+// calculate the differences between the start and end X/Y positions for each of our points
+let delta1x = end1.x - start1.x
+let delta1y = end1.y - start1.y
+let delta2x = end2.x - start2.x
+let delta2y = end2.y - start2.y
 
+// create a 2D matrix from our vectors and calculate the determinant
+let determinant = delta1x * delta2y - delta2x * delta1y
+
+if abs(determinant) < 0.0001 {
+    // if the determinant is effectively zero then the lines are parallel/colinear
+    return nil
+}
+
+// if the coefficients both lie between 0 and 1 then we have an intersection
+let ab = ((start1.y - start2.y) * delta2x - (start1.x - start2.x) * delta2y) / determinant
+
+if ab > 0 && ab < 1 {
+    let cd = ((start1.y - start2.y) * delta1x - (start1.x - start2.x) * delta1y) / determinant
+
+    if cd > 0 && cd < 1 {
+        // lines cross – figure out exactly where and return it
+        let intersectX = start1.x + ab * delta1x
+        let intersectY = start1.y + ab * delta1y
+        return (intersectX, intersectY)
+    }
+}
+
+// lines don't cross
+return nil
+}
 
--- Swift Student Challenge-XCode/SKScenes/GameView.swift
+++ Swift Student Challenge-XCode/SKScenes/GameView.swift
@@ -24,14 +24,10 @@ class GameScene: SKScene {
     var menuElements = [SKNode]()
     
     //Immediately after leveTimerValue variable is set, update label's text
-    private let timer = SKLabelNode(text: "The game starts right on!")
-    private var counter = 20 {
+    let timer = SKLabelNode(text: "The game starts right on!")
+    var counter = 20 {
         didSet {
             self.timer.text = "Time left: \(self.counter) ⏳"
-            if counter < 0 {
-                self.isPaused = true
-                self.timer.text = "You lost 😔"
-            }
             if currentLevel >= 3{
                 self.timer.text = "You are getting realy good at it 😉 \n Time: \(self.counter)"
             }
@@ -65,9 +61,9 @@ class GameScene: SKScene {
     }
     
     override func didMove(to view: SKView) {
-        //        scoreLevelLabel = generateScoreLevelLabel(score: score, currentLevel: currentLevel)
-        //        scoreLevelLabel.zPosition = 2
-        //        addChild(scoreLevelLabel)
+        scoreLevelLabel = generateScoreLevelLabel(score: score, currentLevel: currentLevel)
+        scoreLevelLabel.zPosition = 2
+        addChild(scoreLevelLabel)
         levelUp()
         
     }
@@ -79,6 +75,38 @@ class GameScene: SKScene {
         return CGPoint(x: xPosition, y: yPosition)
     }
     
+    func linesCross(start1: CGPoint, end1: CGPoint, start2: CGPoint, end2: CGPoint) -> CGPoint? {
+    // calculate the differences between the start and end X/Y positions for each of our points
+    let delta1x = end1.x - start1.x
+    let delta1y = end1.y - start1.y
+    let delta2x = end2.x - start2.x
+    let delta2y = end2.y - start2.y
+
+    // create a 2D matrix from our vectors and calculate the determinant
+    let determinant = delta1x * delta2y - delta2x * delta1y
+
+    if abs(determinant) < 0.0001 {
+        // if the determinant is effectively zero then the lines are parallel/colinear
+        return nil
+    }
+
+    // if the coefficients both lie between 0 and 1 then we have an intersection
+    let ab = ((start1.y - start2.y) * delta2x - (start1.x - start2.x) * delta2y) / determinant
+
+    if ab > 0 && ab < 1 {
+        let cd = ((start1.y - start2.y) * delta1x - (start1.x - start2.x) * delta1y) / determinant
+
+        if cd > 0 && cd < 1 {
+            // lines cross – figure out exactly where and return it
+            let intersectX = start1.x + ab * delta1x
+            let intersectY = start1.y + ab * delta1y
+            return CGPoint(x: intersectX, y: intersectY)
+        }
+    }
+
+    // lines don't cross
+    return nil
+    }
     func levelClear() -> Bool {
         var allLines = [Line]()
         
@@ -87,8 +115,8 @@ class GameScene: SKScene {
         }
         for line in allLines {
             for other in allLines {
-                if other.line.intersects(line.line) && line.start != other.end && line.end != other.start && line.line != other.line {
-                    print("intersect")
+                if linesCross(start1: line.start, end1: line.end, start2: other.start, end2: other.end) != nil{
+                    print(linesCross(start1: line.start, end1: line.end, start2: other.start, end2: other.end) as Any)
                     return false
                 }
             }
@@ -214,11 +242,11 @@ class GameScene: SKScene {
                     
                 }
                 else if button.name == "RightButton"{
-                        //roght button action
+                    //roght button action
                     generateNewLevel(newIntrest: true)
                     counter = 20
-                    }
                 }
+            }
             for level in levels {
                 for person in level.persons{
                     if touchedNode.contains(person) {
@@ -252,6 +280,10 @@ class GameScene: SKScene {
     }
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        if counter < 0 {
+            self.isPaused = true
+            self.timer.text = "You lost 😔"
+        }
     }
     func redrawLines(){
         
@@ -285,7 +317,7 @@ class GameScene: SKScene {
     func checkMove() {
         //TODO: Zrobić animiację
         if levelClear() {
-            score += currentLevel * 2
+            score = currentLevel * counter
             self.isUserInteractionEnabled = false
             //            let fadeout = SKAction.fadeOut(withDuration: 0.5)
             //            let fadein = SKAction.fadeIn(withDuration: 0.5)
@@ -323,7 +355,7 @@ class GameScene: SKScene {
         }
         
         if newIntrest{
-    
+            
             repeat{
                 newColor = HobbiesString.randomElement()!
             }while existingColors.contains(newColor)
